bb41972f2236554b1b538d5347b3d3f344f7d07f
MagaDev <magahr123@gmail.com>
2024-08-03 20:38:13 -0400

    My commit hack 4

--- hack_4.py
+++ hack_4.py
@@ -9,5 +9,12 @@ text: "qux" output => "qux"
 
 def fn_hack_4(s):
     result = s
-    #...
+    for leter0 in result:
+        if leter0 == "f" or leter0 == "b" or leter0 == "n":
+           result  = result.replace(leter0,"") 
     return result
+    
+
+test4 = fn_hack_4("barziman")
+
+print(test4)
