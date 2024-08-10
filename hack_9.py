4e69a141b85fa5364c0acf4d23db90336770684e
MagaDev <magahr123@gmail.com>
2024-08-10 10:50:40 -0400

    My first commit - hack_9

--- hack_9.py
+++ hack_9.py
@@ -4,6 +4,14 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 
 def fn_hack_9(s):
-    result = s
-    #...
-    return result
+    text = s
+    output = {}
+    for key, value in text.items():
+        if key.lower() == "foo":
+            output["Foo"] = value.capitalize()[0:3] + value[4:9]
+    return output
+
+
+test9 = fn_hack_9({"foo": "fookziman", "bar": "barziman"})
+
+print(test9)
