22699c7abeaca7b087f0ad3e828ad9b6245677b9
MagaDev <magahr123@gmail.com>
2024-08-10 11:51:52 -0400

    My first commit - hack 10

--- hack_10.py
+++ hack_10.py
@@ -1,9 +1,16 @@
 """
 text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 """
+def fn_hack_10(text):
+    output = []
+    count = 1
+    for item in text:
+        formatted_item = {}
+        formatted_item[str(count)] = str(count + 1)
+        count += 2
+        output.append(formatted_item)
+       
+    return output
 
-
-def fn_hack_10(s):
-    result = s
-    #...
-    return result
+test10 = fn_hack_10([{"a":"b"},{"c","d"},{"e":"f"}])
+print(test10)
