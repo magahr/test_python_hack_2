c460799dc6d8f1088066081c0792837eb091713b
MagaDev <magahr123@gmail.com>
2024-08-09 22:08:51 -0400

    My first commit - hack 7

--- hack_7.py
+++ hack_7.py
@@ -11,5 +11,19 @@ text: [0] output => [0]
 
 def fn_hack_7(s):
     result = s
-    #...
+    text_03 = []
+    text_01 = ["a","b","c","d","e"]
+    text_02 = ["1", 2 ,"3", 4 ,"5"]
+    for i in range(0, len(s)):
+      for y in range(0, len(text_01)):
+        if s[i] == text_01[y]:
+           text_03.append(text_02[y])   
+
+    if text_03 == []:
+       text_03.append(0)       
+    result = text_03
     return result
+
+test7 = fn_hack_7("d")
+
+print(test7)
