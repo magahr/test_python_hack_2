c97c56eae5f43e05708c4948c6c450170cdb8cbc
MagaDev <magahr123@gmail.com>
2024-08-10 09:56:37 -0400

    My first commit - hack 8

--- hack_8.py
+++ hack_8.py
@@ -9,41 +9,19 @@ text: ["a","b"]             output => ["2","1"]
 
 
 def fn_hack_8(s):
-    result = s
-    text_03 = []
-
-    text_01 = ["a",  "b",  "c",  "d",  "e"]
-    text_02 = ["e-5","d-4","c-3","b-2","a-1"]
-
-    text_04 = ["a",  "b",  "c"]
-    text_05 = ["c-3","b-2","a-1"]
-
-    text_06 = ["a",  "b",  "c",  "d"]
-    text_07 = ["4","3","2","1"]
-
-    text_08 = ["a",  "b"]
-    text_09 = ["2","1"]
-
    
-    for i in range(0, len(s)):
-      if len(s)  == 5:     
-          for y in range(0, len(text_01)):
-              if s[i] == text_01[y]:
-                 text_03.append(text_02[y])   
-      elif len(s)  == 3:
-           for y in range(0, len(text_04)):
-              if s[i] == text_04[y]:
-                 text_03.append(text_05[y]) 
-      elif len(s)  == 4:
-           for y in range(0, len(text_06)):
-              if s[i] == text_06[y]:
-                 text_03.append(text_07[y]) 
-      elif len(s)  == 2:
-           for y in range(0, len(text_08)):
-              if s[i] == text_08[y]:
-                 text_03.append(text_09[y]) 
-    
-    result = text_03
+    text = s
+    output = []
+    count = len(text)
+    for i in range(count, 0, -1):
+        # if i == count:
+        if count % 2 == 0:
+            # output.append(f'{text[i-1]}-{i}')
+            output.append(f'{i}')
+        else:
+            # output.append(f'{text[i-1]}-{i+1}')
+            output.append(f'{text[i-1]}-{i}')
+    result = output
 
     return result
 
